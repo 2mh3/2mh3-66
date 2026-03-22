39e3807ae02089bf252d17438c493bef0df100b8
t-wada <t-wada@c-sticks.com>
2026-03-23 01:53:54 +0900

    remove

--- config.h
+++ config.h
@@ -1,22 +1,17 @@
-/*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
+#include "config_common.h"
 
-// Use the lily version to get the Lily58 logo instead of the qmk logo
-#define OLED_FONT_H "lib/glcdfont_lily.c"
+/* 4x7 + 親指4 (5行7列) のピン設定 */
+#define MATRIX_ROWS 10 // 5行 × 左右2
+#define MATRIX_COLS 7
+
+#define MATRIX_ROW_PINS {D4, C6, D7, E6, B4}
+#define MATRIX_COL_PINS {F4, F5, F6, F7, B1, B3, B2}
+
+/* 分割・通信設定 */
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D1
+#define MASTER_LEFT
+
+/* ダイオードの向き */
+#define DIODE_DIRECTION COL2ROW
